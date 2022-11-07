85e4cff1c34eb4e27113d4dac7968671dfb120b6
radiation22 <radiationcorporation2@gmail.com>
2022-11-07 23:59:59 +0600

    add footer section

--- src/pages/Home/Home/Banner.js
+++ src/pages/Home/Home/Banner.js
@@ -1,53 +1,21 @@
 import React from "react";
-import banner1 from "../../../assets/banner1.jpg";
-import banner2 from "../../../assets/banner3.jpg";
-import banner3 from "../../../assets/banner2.jpg";
+import "./Banner.css";
 
 const Banner = () => {
   return (
-    <div className="carousel w-full">
-      <div id="slide1" className="carousel-item relative w-full">
-        <img src={banner1} className="w-full h-3/4" />
-        <div className="absolute flex justify-between  left-5 right-5 top-1/2">
-          <a href="#slide4" className="btn btn-circle">
-            ❮
-          </a>
-          <a href="#slide2" className="btn btn-circle">
-            ❯
-          </a>
-        </div>
-      </div>
-      <div id="slide2" className="carousel-item relative w-full">
-        <img src={banner2} className="w-full h-3/4" />
-        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-          <a href="#slide1" className="btn btn-circle">
-            ❮
-          </a>
-          <a href="#slide3" className="btn btn-circle">
-            ❯
-          </a>
-        </div>
-      </div>
-      <div id="slide3" className="carousel-item relative w-full">
-        <img src={banner3} className="w-full h-3/4" />
-        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-          <a href="#slide2" className="btn btn-circle">
-            ❮
-          </a>
-          <a href="#slide4" className="btn btn-circle">
-            ❯
-          </a>
-        </div>
-      </div>
-      <div id="slide4" className="carousel-item relative w-full">
-        <img src={banner1} className="w-full h-3/4" />
-        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-          <a href="#slide3" className="btn btn-circle">
-            ❮
-          </a>
-          <a href="#slide1" className="btn btn-circle">
-            ❯
-          </a>
+    <div className="hero min-h-screen banner-section">
+      <div className="hero-overlay bg-opacity-70"></div>
+      <div className="hero-content text-center text-neutral-content">
+        <div className="max-w-md">
+          <h1 className="mb-5 text-5xl font-bold">
+            Capture Love, Joy & Everything With Us
+          </h1>
+          <p className="mb-5">
+            It is a long established fact that a reader will be distracted by
+            the reale he point of using Lorem Ipsum is that it has a
+            more-or-less normal valid.
+          </p>
+          <button className="btn btn-secondary">Get Started</button>
         </div>
       </div>
     </div>
